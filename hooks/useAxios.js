be6e7bd17d6468e25928b0c65d4682bc721ba212
charsijawaan <shakeelusama3@gmail.com>
2022-03-25 14:27:33 +0500

    add: tokens refresh

--- hooks/useAxios.js
+++ hooks/useAxios.js
@@ -3,14 +3,51 @@ import axios from 'axios'
 const useAxios = () => {
 	const axiosInstance = axios.create({
 		baseURL: process.env.NEXT_PUBLIC_API,
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization:
-				typeof window !== 'undefined'
-					? `Bearer ${localStorage.getItem('accessToken')}`
-					: '',
-		},
 	})
+
+	axiosInstance.interceptors.request.use(
+		async (config) => {
+			config.headers = {
+				'Content-Type': 'application/json',
+				Authorization:
+					typeof window !== 'undefined'
+						? `Bearer ${localStorage.getItem('accessToken')}`
+						: '',
+			}
+			return config
+		},
+		(error) => {
+			Promise.reject(error)
+		}
+	)
+
+	axiosInstance.interceptors.response.use(
+		(response) => {
+			return response
+		},
+		async function (error) {
+			const originalRequest = error.config
+			if (error.response.status === 403 && !originalRequest._retry) {
+				originalRequest._retry = true
+				const res = await fetch(`${process.env.NEXT_PUBLIC_API}/token`, {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						refreshToken: localStorage.getItem('refreshToken'),
+					}),
+				})
+				const content = await res.json()
+				const accessToken = content.accessToken
+				localStorage.setItem('accessToken', accessToken)
+				axios.defaults.headers.common['Authorization'] = 'Bearer ' + accessToken
+				return axiosInstance(originalRequest)
+			}
+			return Promise.reject(error)
+		}
+	)
+
 	return axiosInstance
 }
 
