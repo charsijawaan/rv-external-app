af97338bd792c3b7e6ac3e7c4530b3e5c692f4b2
charsijawan <shakeelusama3@gmail.com>
2022-03-27 14:57:27 +0500

    commit

--- components/CancelTripModal/index.js
+++ components/CancelTripModal/index.js
@@ -1,80 +1,84 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const CancelTripModal = ({ setShowModal, onCancelReservation }) => {
-	return (
-		<>
-			<div className='fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none'>
-				<div className='relative w-auto max-w-3xl mx-auto my-6'>
-					{/* Content */}
-					<div className='relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none'>
-						{/* Header */}
-						<div className='flex items-start justify-between p-5 border-b border-solid rounded-t border-blueGray-200 '>
-							<svg
-								className='mx-auto mb-4 ml-[10em] mt-5 w-14 h-14 text-gray-400 dark:text-gray-200'
-								fill='none'
-								stroke='currentColor'
-								viewBox='0 0 24 24'
-								xmlns='http://www.w3.org/2000/svg'
-							>
-								<path
-									stroke-linecap='round'
-									stroke-linejoin='round'
-									stroke-width='2'
-									d='M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-								></path>
-							</svg>
+const CancelTripModal = ({
+  setShowModal,
+  onCancelReservation,
+  reservationId,
+}) => {
+  return (
+    <>
+      <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+        <div className="relative w-auto max-w-3xl mx-auto my-6">
+          {/* Content */}
+          <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
+            {/* Header */}
+            <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-blueGray-200 ">
+              <svg
+                className="mx-auto ml-[10em] mt-2 w-14 h-10 text-gray-400 dark:text-gray-200"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
 
-							<button
-								type='button'
-								className='text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white'
-								data-modal-toggle='popup-modal'
-								onClick={() => setShowModal(false)}
-							>
-								<svg
-									className='w-5 h-5'
-									fill='currentColor'
-									viewBox='0 0 20 20'
-									xmlns='http://www.w3.org/2000/svg'
-								>
-									<path
-										fill-rule='evenodd'
-										d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
-										clip-rule='evenodd'
-									></path>
-								</svg>
-							</button>
-						</div>
-						{/* Body */}
-						<div className='relative flex-auto p-4 text-center'>
-							<p className='my-5 leading-relaxed text-blueGray-500 text-md '>
-								Are you sure you want to delete this reservation?
-							</p>
-						</div>
-						{/* Footer */}
-						<div className='flex items-center justify-end p-6 border-t border-solid rounded-b border-blueGray-200'>
-							<button
-								data-modal-toggle='popup-modal'
-								type='button'
-								className='text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2'
-								onClick={() => onCancelReservation(r._id)}
-							>
-								Yes, I'm sure
-							</button>
-							<button
-								data-modal-toggle='popup-modal'
-								type='button'
-								className='text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600'
-								onClick={() => setShowModal(false)}
-							>
-								No, cancel
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className='fixed inset-0 z-40 bg-black opacity-25'></div>
-		</>
-	)
-}
+              <button
+                type="button"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                data-modal-toggle="popup-modal"
+                onClick={() => setShowModal(false)}
+              >
+                <svg
+                  className="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+            {/* Body */}
+            <div className="relative flex-auto p-4 text-center">
+              <p className="my-5 leading-relaxed text-blueGray-500 text-md ">
+                Are you sure you want to delete this reservation?
+              </p>
+            </div>
+            {/* Footer */}
+            <div className="flex items-center justify-end p-4 border-t border-solid rounded-b border-blueGray-200">
+              <button
+                data-modal-toggle="popup-modal"
+                type="button"
+                className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-2.5 py-2.5 text-center mr-2"
+                onClick={() => onCancelReservation(reservationId)}
+              >
+                Yes, I'm sure
+              </button>
+              <button
+                data-modal-toggle="popup-modal"
+                type="button"
+                className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-2.5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                onClick={() => setShowModal(false)}
+              >
+                No, cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+    </>
+  );
+};
 
-export default CancelTripModal
+export default CancelTripModal;
--- services/camper/index.js
+++ services/camper/index.js
@@ -1,67 +1,75 @@
-import useAxios from '../../hooks/useAxios'
+import useAxios from "../../hooks/useAxios";
 
-const axios = useAxios()
+const axios = useAxios();
 
 export const addNewCard = async (data) => {
-	try {
-		return await axios.post('/camper/add_new_card', data)
-	} catch (err) {
-		return err.response
-	}
-}
+  try {
+    return await axios.post("/camper/add_new_card", data);
+  } catch (err) {
+    return err.response;
+  }
+};
 
 export const getCardsByCamperId = async (camperId) => {
-	try {
-		return await axios.get(`/camper/get_cards?camperId=${camperId}`)
-	} catch (err) {
-		return err.response
-	}
-}
+  try {
+    return await axios.get(`/camper/get_cards?camperId=${camperId}`);
+  } catch (err) {
+    return err.response;
+  }
+};
 
 export const reserveSite = async (data) => {
-	try {
-		return await axios.post('/camper/reserve', data)
-	} catch (err) {
-		return err.response
-	}
-}
+  try {
+    return await axios.post("/camper/reserve", data);
+  } catch (err) {
+    return err.response;
+  }
+};
 
 export const deleteCard = async (data) => {
-	try {
-		return await axios.post('/camper/credit_card', data)
-	} catch (err) {
-		return err.response
-	}
-}
+  try {
+    return await axios.post("/camper/credit_card", data);
+  } catch (err) {
+    return err.response;
+  }
+};
 
 export const getTrips = async (camperId) => {
-	try {
-		return await axios.get(`/camper/get_trips?camperId=${camperId}`)
-	} catch (err) {
-		return err.response
-	}
-}
+  try {
+    return await axios.get(`/camper/get_trips?camperId=${camperId}`);
+  } catch (err) {
+    return err.response;
+  }
+};
 
 export const cancelReservation = async (data) => {
-	try {
-		return await axios.post('/camper/cancel_reservation', data)
-	} catch (err) {
-		return err.response
-	}
-}
+  try {
+    return await axios.post("/camper/cancel_reservation", data);
+  } catch (err) {
+    return err.response;
+  }
+};
+
+export const submitReview = async (data) => {
+  try {
+    return await axios.post("/reservation/review", data);
+  } catch (err) {
+    return err.response;
+  }
+};
 
 export const updateAdults = async (data) => {
-	try {
-		return await axios.post('/camper/update_adults', data)
-	} catch (err) {
-		return err.response
-	}
-}
+  try {
+    return await axios.post("/camper/update_adults", data);
+  } catch (err) {
+    return err.response;
+  }
+};
 
 export const updateChildren = async (data) => {
-	try {
-		return await axios.post('/camper/update_children', data)
-	} catch (err) {
-		return err.response
-	}
-}
+  try {
+    return await axios.post("/camper/update_children", data);
+  } catch (err) {
+    return err.response;
+  }
+};
